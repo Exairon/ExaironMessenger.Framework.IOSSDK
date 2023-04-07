964735f5f30990a86d9b70babf9200dda5d6460e
Enes Toprak <etoprak174@gmail.com>
2023-04-07 09:19:51 +0300

    Add framework

---
+++ ExaironMessenger.Framework.IOSSDK/ExaironMessenger.Framework.IOSSDK/ExaironMessenger_Framework_IOSSDK.h
@@ -0,0 +1,18 @@
+//
+//  ExaironMessenger_Framework_IOSSDK.h
+//  ExaironMessenger.Framework.IOSSDK
+//
+//  Created by Exairon on 7.04.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExaironMessenger_Framework_IOSSDK.
+FOUNDATION_EXPORT double ExaironMessenger_Framework_IOSSDKVersionNumber;
+
+//! Project version string for ExaironMessenger_Framework_IOSSDK.
+FOUNDATION_EXPORT const unsigned char ExaironMessenger_Framework_IOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExaironMessenger_Framework_IOSSDK/PublicHeader.h>
+
+
